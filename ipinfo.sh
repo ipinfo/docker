0ed28375b9ba7a4868e9a73fa7c99374ab246e41
Vincent Bernat <vincent@bernat.ch>
2024-06-04 19:06:44 +0200

    gracefully stop on TERM signal
    When not being on foreground, a shell script is not leader of its
    process group and the TERM signal does not kill the commands running as
    children. This changes ensure the command in the foreground is a shell
    builtin (wait).

--- ipinfo.sh
+++ ipinfo.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+trap exit TERM
+
 while true; do
     for DATABASE in ${IPINFO_DATABASES}; do
         RESPONSE=$(curl \
@@ -18,5 +20,6 @@ while true; do
         break
     fi
 
-    sleep "$UPDATE_FREQUENCY"
+    sleep "$UPDATE_FREQUENCY" &
+    wait $!
 done
